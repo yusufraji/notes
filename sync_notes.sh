36470c38a389ef252dafd3df950d146dace1d29f
yusufraji <raji.yusuf234@gmail.com>
2021-12-19 23:37:50 +0800

    Modify sync_notes script

--- sync_notes.sh
+++ sync_notes.sh
@@ -4,7 +4,6 @@ gitstatus="$(git status --porcelain | wc -l)"
 
 if [ "$gitstatus" -ne 0 ]
 then
-    echo "$gitstatus"
 
     git add --all
     git commit -m "Last sync: $(date +"%Y-%m-%d %H:%M:%S")"
